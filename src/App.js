ee367f399a1c42b9c3c692958c8790168d109692
sohan653 <sohanurl653@gmail.com>
2023-04-07 12:37:24 +0600

    solve git merge problem

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MiddleNav from "./Component/navbar/MiddleNav";
 import TopNav from "./Component/navbar/TopNav";
+import MiddleNav from "./Component/navbar/MiddleNav";
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <header>
-        <TopNav></TopNav>
-        <MiddleNav></MiddleNav>
-      </header>
-    </BrowserRouter>
-  );
-};
 
-import {React} from 'react';
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
-import Navbar from "./Component/navbar/Navbar";
-import {Toaster} from "react-hot-toast";
-import Dashbord from './Component/Dashboard/Dashbord';
-import Register from './Pages/Auth/Register';
-import Login from './Pages/Auth/Login';
-import SendOtp from "./Pages/Auth/SendOtp";
-import ChangePass from "./Pages/Auth/ChangePass";
-import OtpVerify from './Pages/Auth/OtpVerify';
 
 
 
@@ -33,20 +12,9 @@ const App = () => {
 
       return (
           <>
-              <BrowserRouter>
-                  {/* <Navbar/> */}
-                  <Toaster position="top-center"/>
-                  <Routes>
-                  
-                      <Route path='/login' element={<Login/>} ></Route>                      
-                      <Route path='/register' element={<Register/>} ></Route>
-                      <Route path='/sendotp' element={<SendOtp/>} ></Route>                      
-                      <Route path='/verifiotp' element={<OtpVerify/>} ></Route>
-                      <Route path='/changepassword' element={<ChangePass/>} ></Route>
-                  </Routes>
-
-              </BrowserRouter>
-             
+            <TopNav></TopNav>
+              <MiddleNav></MiddleNav>
+             hi
           </>
       )
   }
