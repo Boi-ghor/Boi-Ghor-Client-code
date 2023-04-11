04feff657a129e6adb4a22d8504abb5a5f22c67a
dip_sarker <sarkerdip005@gmail.com>
2023-04-11 11:08:34 +0600

    product table

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Hero from "../../Component/HomePage/Hero";
+import PopuBooks from '../../Component/HomePage/PopuBooks';
 
 const HomePage = () => {
     return (
         <div>
            <Hero></Hero>
+
+           <PopuBooks/>
         </div>
     );
 };
