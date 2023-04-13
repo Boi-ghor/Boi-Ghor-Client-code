abf34fb9fa80a4d87a801f43f7b0bbd3ef520890
sumitxt <sumitbarua14@gmail.com>
2023-04-13 21:59:33 +0600

    author

--- src/Component/Card/AuthorCard.js
+++ src/Component/Card/AuthorCard.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import author from "../../Assets/img/author.jpg"
 const AuthorCard = () => {
     return (
-        <div>
-            round card for author
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body items-center text-center">
+                <div class="avatar">
+                    <div class="w-24 rounded-full">
+                        <img src={author} />
+                    </div>
+                </div>
+                <h2 className="card-title">Jessica!</h2>
+                <p>buy books from this famous author</p>
+                <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Component/HomePage/TopAuthors.js
+++ src/Component/HomePage/TopAuthors.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import AuthorCard from '../Card/AuthorCard';
+const author=[1,2,3,4,5,6]
 
 const TopAuthors = () => {
     return (
         <div>
-            top authors
-        </div>
+        <h1 className='text-3xl font-semibold mx-10 my-6'>Popular Books</h1>
+
+      <div className='flex flex-wrap justify-center gap-6'> 
+      {author.map ((i)=>{
+            {console.log(i)}
+            
+
+            return < AuthorCard/>
+        }
+            
+        
+
+        )}
+
+      </div>
+       
+    </div>
     );
 };
 
