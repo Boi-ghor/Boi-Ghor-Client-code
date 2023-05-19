66b2f669e8d23a005da69530b1a5f282538fe5c2
sumitxt <sumitbarua14@gmail.com>
2023-05-19 23:06:14 +0600

    redesigned details of author publisher

--- src/Component/Details/PublishersComponent.js
+++ src/Component/Details/PublishersComponent.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { LoaderContext } from '../../context/loader';
 import { LoadAll } from '../../ApiRequest/ApiRequest';
 import BookCardAlt from '../Card/BookCardAlt';
+import {BsRss} from "react-icons/bs";
 
 const PublishersComponent = ({ publisher }) => {
     const [books, setBooks] = useState([]);
@@ -34,25 +35,35 @@ const PublishersComponent = ({ publisher }) => {
 
     return (
         <div className="container mx-auto py-10">
-            <section className="bg-white py-10">
-                <div className="container mx-auto px-4">
-                    <div className="text-center">
-                        <div className="flex justify-center">
-                            <div className="w-1/2">
-                                <img className="mx-auto rounded-full w-32 h-32 object-cover"
-                                     src={photoURL}
-                                     alt="publisherName"/>
+            <section className="py-10">
+                <div className="grid gap-6 shadow-md">
+                    <div className="col-span-8 xl:col-span-9 bg-white rounded-tl-lg rounded-bl-lg">
+                        <div className="grid grid-cols-12">
+                            <div className="col-span-5 xl:col-span-4 p-6">
+                                <div>
+                                    <img className="w-full object-cover" src={photoURL} alt={publisherName} />
+                                </div>
+                            </div>
+                            <div className="col-span-7 xl:col-span-8 p-6 pl-0">
+                                <div>
+                                    <h2 className="capitalize text-6xl mb-1 py-5">{publisherName}</h2>
+
+
+                                    <p className="flex text-2xl gap-2 mb-10">
+                    <span className="pt-1">
+                      <BsRss size={24} />
+                    </span>
+                                        {aboutPublisher}
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                        <h1 className="text-4xl font-bold text-gray-800 mb-5">{publisherName}</h1>
-                        <p className="text-xl text-gray-600 mb-5">{aboutPublisher}</p>
-
                     </div>
                 </div>
             </section>
 
+            <div className="p-6 py-7 bg-white shadow mt-16 rounded-lg">
             <h2 className="text-3xl font-bold text-gray-800 mt-10 mb-5">Books</h2>
-
             <div className="grid gap-4 lg:grid-cols-4 md:grid-cols-3">
                 {books.map((book) => (
                     <div key={book?._id}>
@@ -60,6 +71,7 @@ const PublishersComponent = ({ publisher }) => {
                     </div>
                 ))}
             </div>
+        </div>
         </div>
     );
 };
