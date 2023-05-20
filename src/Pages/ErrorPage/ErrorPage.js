d57e2273721261207456cba70fe4b3ae0bea11cf
sumitxt <sumitbarua14@gmail.com>
2023-05-21 01:04:13 +0600

    error page

--- src/Pages/ErrorPage/ErrorPage.js
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useLottie } from "lottie-react";
+import error from "../../Assets/lottie/page-not-found-404.json";
+import error2 from "../../Assets/lottie/404-page-animation.json";
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
+    const options = {
+        animationData: error2,
+        loop: true
+    };
+
+    const { View } = useLottie(options);
+
     return (
-        <div>
-            errorpage
+        // <div className="flex flex-col items-center justify-center bg-[#FDE3FD]">
+        <div className="flex flex-col items-center justify-center">
+            <div className="mb-4">{View}</div>
+            <div className="mb-4">
+                <Link to="/" className="btn btn-primary btn-lg">
+                    Return to Home
+                </Link>
+            </div>
         </div>
     );
 };
