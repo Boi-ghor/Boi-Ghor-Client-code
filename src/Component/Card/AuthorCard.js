c587642b55befe1cd7fb6b519c461b60aa2c558a
sumitxt <sumitbarua14@gmail.com>
2023-05-23 01:16:17 +0600

    Top Top

--- src/Component/Card/AuthorCard.js
+++ src/Component/Card/AuthorCard.js
@@ -1,32 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const AuthorCard = ({ author }) => {
-  const { id, name, books } = author;
+
+const AuthorCard = ({ author, books }) => {
+  const { authorName, photoURL } = author;
 
   return (
-    <div className="bookCard w-full h-full">
-      <div className="flex flex-col h-full">
-        <div className="flex justify-center">
-          <img
-            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-            alt={name}
-            className="authorImg w-44 max-w-full h-44 rounded-full"
-          />
-        </div>
+      <div className="bookCard w-full h-full">
+        <div className="flex flex-col h-full">
+          <div className="flex justify-center">
+            <img
+                src={photoURL}
+                alt={authorName}
+                className="authorImg w-44 max-w-full h-44 rounded-full"
+            />
+          </div>
 
-        <h4 className="font-semibold mt-6 my-4 sm:my-8">{name}</h4>
+          <h4 className="font-semibold mt-6 my-4 sm:my-8">{authorName}</h4>
 
-        <div className="max-w-xs mx-auto mt-auto px-3 py-2 bg-red-100 w-full rounded border-red-200 shadow flex items-center justify-between">
-          <h5 className="font-medium">Books: {books.length}</h5>
-          <Link
-            to={`/author/${id}`}
-            className="btn btn-primary btn-sm md:btn-md min-h-8 h-10 rounded capitalize"
-          >
-            View Details
-          </Link>
+          <div className="max-w-xs mx-auto mt-auto px-3 py-2 bg-red-100 w-full rounded border-red-200 shadow flex items-center justify-between">
+            <h5 className="font-medium">Books: {books?.length || 0}</h5>
+            <Link
+                to={`/author/${author._id}`}
+                className="btn btn-primary btn-sm md:btn-md min-h-8 h-10 rounded capitalize"
+            >
+              View Details
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
--- src/Component/Card/PublisherCard.js
+++ src/Component/Card/PublisherCard.js
@@ -1,26 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const PublisherCard = ({ author }) => {
-  const { id, name, books } = author;
+const PublisherCard = ({ publisher, books }) => {
+  const { publisherName, _id, photoURL, sellCount} = publisher;
 
   return (
     <div className="bookCard w-full h-full">
       <div className="flex flex-col h-full">
         <div className="flex justify-center">
           <img
-            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-            alt={name}
+            src={photoURL}
+            alt={publisherName}
             className="authorImg w-44 max-w-full h-44 rounded-lg"
           />
         </div>
 
-        <h4 className="font-semibold mt-6 my-4 sm:my-8">{name}</h4>
+        <h4 className="font-semibold mt-6 my-4 sm:my-8">{publisherName}</h4>
 
         <div className="max-w-xs mx-auto mt-auto px-3 py-2 bg-red-100 w-full rounded border-red-200 shadow flex items-center justify-between">
-          <h5 className="font-medium">Books: {books.length}</h5>
+          <h5 className="font-medium">Books: {books?.length || 0}</h5>
           <Link
-            to={`/author/${id}`}
+              to={`/publisher/${_id}`}
             className="btn btn-primary btn-sm md:btn-md min-h-8 h-10 rounded capitalize"
           >
             View Details
